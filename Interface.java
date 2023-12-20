56a9c7dcfd27756f709d2c95b7c8fc2cccbc586c
Casquinha2 <tmdscasquinha@gmail.com>
2023-12-20 16:25:13 +0000

    notas com o que e necessario para trabalho

--- Interface.java
+++ Interface.java
@@ -1,1 +1,14 @@
+import javax.swing.*;
 
+public class Interface {
+    private JPanel panel1;
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("teste");
+        frame.setContentPane(new Interface().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+    }}
