d0d0a5001201855f2f5bf96231b8cb97f2dd852c
HFPADROLS <hfpedro2003@gmail.com>
2023-12-20 17:20:04 +0000

    Mnesagem teste

--- Interface.java
+++ Interface.java
@@ -1,8 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Interface {
     private JPanel panel1;
-
+    private JLabel ImageDragao;
+    public Interface(){
+        ImageDragao.setIcon(new ImageIcon(new javax.swing.ImageIcon(getClass().getResource("dragao.png")).getImage().getScaledInstance(1920, 1080, Image.SCALE_SMOOTH)));
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("teste");
@@ -10,5 +14,6 @@ public class Interface {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
 
     }}
