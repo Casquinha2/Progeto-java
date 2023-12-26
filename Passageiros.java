92d88164e3d1254c0adfaa01189de569e960580d
Casquinha2 <tmdscasquinha@gmail.com>
2023-12-26 17:54:01 +0000

    passageiro

--- Passageiros.java
+++ Passageiros.java
@@ -1,5 +1,14 @@
 public class Passageiros {
-    public Passageiros() {
+    private String nome;
+    private int id;
+    private String pais;
+
+    private int lugarreservado;
+
+    public Passageiros(String nome, String pais) {
+        this.nome = nome;
+        this.pais = pais;
 
     }
+
 }
