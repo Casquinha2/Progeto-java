efe1f163b0eab859ab9410b3f9e9bd37a44d3173
HFPADROLS <hfpedro2003@gmail.com>
2023-12-26 17:18:35 +0000

    classes...de classes
    yuh

--- Aviao.java
+++ Aviao.java
@@ -1,2 +1,30 @@
 public class Aviao {
+    private int numeroDeLugares;
+    private String modelo;
+
+    public Aviao() {
+        this.numeroDeLugares = 120;
+        this.modelo = "Modelo";
+    }
+
+    public int getNumeroDeLugares() {
+        return this.numeroDeLugares;
+    }
+
+    public void setNumeroDeLugares(int numeroDeLugares) {
+        this.numeroDeLugares = numeroDeLugares;
+    }
+
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void InformacoesDoAviao() {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Número de lugares: " + this.numeroDeLugares);
+    }
 }
--- Code.java
+++ Code.java
@@ -1,5 +1,20 @@
 public class Code {
     public Code(){
+        Aviao AviaoLMAO = new Aviao();
+
+        AviaoLMAO.setNumeroDeLugares(150);
+        AviaoLMAO.setModelo("Bruh Moment");
+
+        Voos VooSeiLa = new Voos();
+
+        VooSeiLa.setPaisSaida("Tu casa");
+        VooSeiLa.setPaisChegada("MI CASA");
+        VooSeiLa.setHorario("Ainda nao pus configuracoes de horario por isso fica assim lmao");
+        VooSeiLa.setPreco(10000);
+        VooSeiLa.setLugaresReservados(5);
+        VooSeiLa.setLugaresLivres(AviaoLMAO.getNumeroDeLugares() - VooSeiLa.getLugaresReservados());
+        AviaoLMAO.InformacoesDoAviao();
+        VooSeiLa.InformacoesDoVoo();
 
     }
 }
