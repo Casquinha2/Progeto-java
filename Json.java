23c79bf4b9ef4578b7a69f5e08b60a649a9185d0
Casquinha2 <tmdscasquinha@gmail.com>
2023-12-28 20:37:52 +0000

    Json

--- Json.java
+++ Json.java
@@ -1,10 +1,12 @@
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.lang.reflect.Type;
 
@@ -22,10 +24,18 @@ public class Json {
     };
     public Object ler() throws FileNotFoundException {
         try {
+            List<Passageiros> listapessoas = new ArrayList<>();
             FileReader reader = new FileReader("Passageiros.json");
             Type tipo = new TypeToken<List<Passageiros>>(){}.getType();
-            return gson.fromJson(reader, tipo);
+            listapessoas = gson.fromJson(reader, tipo);
+            reader.close();
+            File file = new File("Passageiros.json");
+            file.delete();
+            return listapessoas;
 
+        } catch (FileNotFoundException e) {
+            List<Passageiros> listapessoas = new ArrayList<>();
+            return listapessoas;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
